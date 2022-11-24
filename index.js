443a96640086d0ae23cb1adc3b6d752411002e24
itsmiraz <mirajulislamofficial044@gmail.com>
2022-11-24 16:35:10 +0600

    status update api

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ const port = process.env.PORT || 5000;
 const jwt = require('jsonwebtoken');
 
 
-const catagories = require('./data/catagories.json')
 
 app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fpgnyx0.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -90,6 +89,38 @@ async function run() {
             };
             const bikes = await bikesCollection.find(query).toArray()
             res.send(bikes)
+        })
+
+        // app.get('/bike', async (req, res) => {
+        //     const filter = {}
+        //     const option = { upsert: true }
+        //     const updateDoc = {
+        //         $set: {
+        //             status:'available',
+        //         }
+        //     }
+        //     const result = await bikesCollection.updateMany(filter, updateDoc, option)
+        //     res.send(result)
+        // })
+
+        app.put('/bike/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const option = { upsert: true };
+            const updatedoc = {
+                $set: {
+                    status:'sold',  
+                }
+            }
+            const result = await bikesCollection.updateOne(filter, updatedoc, option);
+            res.send(result)
+        })
+
+        app.delete('/bike/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await bikesCollection.deleteOne(query);
+            res.send(result)
         })
 
     }
